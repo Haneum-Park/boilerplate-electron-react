3a98ae02967a10374fe3041b78785c7e08849e92
Haneum-Park <qkrgksdma17@gmail.com>
2025-02-08 14:24:58 +0900

    [INIT]

--- public/preload.js
+++ public/preload.js
@@ -1,4 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 window.ipcRenderer = ipcRenderer;
-contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer);
+contextBridge.exposeInMainWorld('ipcRenderer', {
+  ...ipcRenderer,
+  on: (channel, func) => {
+    const subscription = (_event, ...args) => func(_event, ...args);
+    ipcRenderer.on(channel, subscription);
+
+    return () => ipcRenderer.removeListener(channel, subscription);
+  },
+});
